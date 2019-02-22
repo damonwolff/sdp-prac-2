fe33d0049b6f3122cbc5ecef516a9022ae79fe31
Damon <damonswolff@gmail.com>
2019-02-22 15:03:03 +0200

    Problems.Tests and updated program added

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 
-namespace Problems
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Problems {
+
+    public class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter some input");
+            string input = Console.ReadLine();
+            Console.WriteLine(Prefix(input));
+        }
+
+        static string Prefix(string input) {
+            int words = 0;
+            int length = input.Length;
+            bool noWords = true;
+            char[] c = input.ToCharArray();
+            bool looking = true; // currently looking for a word
+
+            for(int i = 0; i< length; i++) {
+                Console.WriteLine("curr = " + c[i]);
+                if(c[i] != ' ' && noWords == true) {//checks whether or not there is at least one word
+                    noWords = false;
+                }
+
+                if(looking == true && c[i] == ' ') { // skips over spaces
+                    continue;
+                }
+
+                if(looking == false && c[i] != ' ') {// skips over words that have already been counted
+                    continue;
+                }
+
+                if(looking == false && c[i] == ' ') { // if finds a space after a word, then look for a new word
+                    looking = true;
+                }
+
+                if(looking == true && c[i] != ' ') { //if looking for a new word and finds !space then words++ and stop looking
+                    looking = false;
+                    words++;
+                }
+
+            }
+
+            if(noWords == false && words == 0) {// if a word was found but not added, then add it
+                words++;
+            }
+
+            return Convert.ToString(length) + "," + Convert.ToString(words) + ":" + input;
         }
     }
 }
